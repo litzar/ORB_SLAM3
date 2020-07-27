9fb059212a8c3ebc70f2ad9a16df209e563aeec2
Richard <linliz@qq.com>
2020-07-27 21:02:34 +0800

    MH04 stereo-i test

--- euroc_examples.sh
+++ euroc_examples.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pathDatasetEuroc='/Datasets/EuRoC' #Example, it is necesary to change it by the dataset path
+pathDatasetEuroc='/home/lin/Datasets/EuRoC' #Example, it is necesary to change it by the dataset path
 
 #------------------------------------
 # Monocular Examples
